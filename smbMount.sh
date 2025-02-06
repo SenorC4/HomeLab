c8197a775b37f5c377454b98d161cdfc8610a262
SenorC4 <44325992+SenorC4@users.noreply.github.com>
2025-02-06 12:22:15 -0500

    Update smbMount.sh

--- smbMount.sh
+++ smbMount.sh
@@ -4,5 +4,5 @@ mkdir /mnt/media
 read -p "Enter your username: " username
 read -p "Enter your password: " password
 
-echo "username=$username\npassword=$password" >> /root/.fileserver_smbcredentials 
+echo -e "username=$username\npassword=$password" >> /root/.fileserver_smbcredentials 
 echo "//nas.lukelecain.com/Media /mnt/media cifs rw,vers=3.0,credentials=/root/.fileserver_smbcredentials,dir_mode=0775,file_mode=0775,uid=1000,gid=9999" >> /etc/fstab
