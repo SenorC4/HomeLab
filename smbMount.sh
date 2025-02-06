db62669116f83f8490ad7c310211c0f9b7ee4a68
SenorC4 <44325992+SenorC4@users.noreply.github.com>
2025-02-06 12:09:57 -0500

    Create smbMount.sh

---
+++ smbMount.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+apt install cifs-utils
+mkdir /mnt/media
+echo "username=luke\npassword=Lukejoshua13" >> /root/.fileserver_smbcredentials 
+echo "//nas.lukelecain.com/Media /mnt/media cifs rw,vers=3.0,credentials=/root/.fileserver_smbcredentials,dir_mode=0775,file_mode=0775,uid=1000,gid=9999" >> /etc/fstab
