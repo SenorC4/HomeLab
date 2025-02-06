0e4f991f80e28c96aada7995701c5e6e3314c6f9
SenorC4 <44325992+SenorC4@users.noreply.github.com>
2025-02-06 12:11:28 -0500

    Update smbMount.sh

--- smbMount.sh
+++ smbMount.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 apt install cifs-utils
 mkdir /mnt/media
-echo "username=lukel\npassword=Lukejoshua13" >> /root/.fileserver_smbcredentials 
+read -p "Enter your username: " username
+read -p "Enter your password: " password
+
+echo "username=$username\npassword=$password" >> /root/.fileserver_smbcredentials 
 echo "//nas.lukelecain.com/Media /mnt/media cifs rw,vers=3.0,credentials=/root/.fileserver_smbcredentials,dir_mode=0775,file_mode=0775,uid=1000,gid=9999" >> /etc/fstab
