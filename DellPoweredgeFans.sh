d3c40bad71b1b385b828e8fa0c33a1d35c6dab50
SenorC4 <44325992+SenorC4@users.noreply.github.com>
2025-05-08 19:22:25 -0400

    Update DellPoweredgeFans.sh

--- DellPoweredgeFans.sh
+++ DellPoweredgeFans.sh
@@ -2,10 +2,10 @@
 #ipmitool -I lanplus -H <iDRAC-IP> -U <iDRAC-USER> -P <iDRAC-PASSWORD> raw 0x30 0x30 0x01 0x01
 
 # set fan controll to manual mode
-ipmitool -I lanplus -H 192.168.0.120 -U root -P calvin raw 0x30 0x30 0x01 0x00
+ipmitool -I lanplus -H 192.168.1.19 -U root -P calvin raw 0x30 0x30 0x01 0x00
 
 # Set fan speed to hex 0x14 == 20% fan speed
-ipmitool -I lanplus -H 192.168.0.120 -U root -P calvin raw 0x30 0x30 0x02 0xff 0x14
+ipmitool -I lanplus -H 192.168.1.19 -U root -P calvin raw 0x30 0x30 0x02 0xff 0x17
 
 # set fan speed to 100 %
 #ipmitool -I lanplus -H <iDRAC-IP> -U <iDRAC-USER> -P <iDRAC-PASSWORD> raw 0x30 0x30 0x02 0xff 0x64
